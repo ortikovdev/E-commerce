eeb87a2f49693ed99f4f75470fe64eeac65a6463
anvardev <oranwhatever@gmail.com>
2024-05-03 15:08:42 +0500

    auth urls have planned

--- apps/account/urls.py
+++ apps/account/urls.py
@@ -1,10 +1,24 @@
 """
-1 - Submit email form                         //PasswordResetView.as_view()
-2- Email sent success message                 //PasswordResetDoneView.as_view()
-3- Link to password Rest form in email        //PasswordResetConfirmView.as_view()
-4- Password successfully changed message      //PasswordResetCompleteView.as_view()
+Register:
+    - /register/ -> send code to user via email
+    - /verify/email/ -> verify code -> activate and give token
+Login:
+    - /login/ -> give token
+Verify Account:
+    - /send/mail -> send code to user via email
+    - /verify/mail -> verify code -> activate and give token
+Change Password:
+    - /change/password/ -> change password
+Reset Password:
+        - /send/mail -> send code to user via email
+        - /verify/mail -> verify code -> activate and give token
+    - /reset/password/ -> reset password
 """
-# from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView
-#
-# PasswordResetView.as_view()
-# PasswordResetDoneView.as_view()
+
+# reset password
+"""
+    1- Submit email form                          //PasswordResetView.as_view()
+    2- Email sent success message                 //PasswordResetDoneView.as_view()
+    3- Link to password Rest form in email        //PasswordResetConfirmView.as_view()
+    4- Password successfully changed message      //PasswordResetCompleteView.as_view()
+"""
